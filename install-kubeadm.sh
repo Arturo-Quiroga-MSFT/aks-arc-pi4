397746af0a032c1037e066cda265dae494567ec3
Arturo R. Quiroga <arturoqu@hotmail.ca>
2020-12-18 13:13:18 -0500

    added kubeinit materials

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -1,6 +1,6 @@
-# script to deploy pre-requisites for kubeadm
-# to install kubeadm
-# to create a cluster with kubeadm
+# Script to deploy pre-requisites for kubeadm
+# 1. to install kubeadm
+# 2. to create a cluster with kubeadm
 
 # Start with steps from https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 
@@ -31,7 +31,7 @@ sudo systemctl restart kubelet
 
 
 # continue with steps from https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/
-# first install a CNI, like CALICO ==> https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#pod-network
+# first install a CNI, see overview ==> https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/create-cluster-kubeadm/#pod-network
 
 # Take care that your Pod network must not overlap with any of the host networks: you are likely to see problems if there is any overlap. 
 # (If you find a collision between your network plugin's preferred Pod network and some of your host networks, you should think of a 
@@ -41,4 +41,35 @@ sudo systemctl restart kubelet
 
 # By default, kubeadm sets up your cluster to use and enforce use of RBAC (role based access control). 
 # Make sure that your Pod network plugin supports RBAC, and so do any manifests that you use to deploy it.
+
+# You can see all other CNIs in ==> https://kubernetes.io/docs/concepts/cluster-administration/networking/#calico
+# If you choose CALICO, then follow these instructions ==> https://docs.projectcalico.org/getting-started/kubernetes/quickstart
+
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
+kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
+
+watch kubectl get pods -n calico-system
+
+# Optional step to allow scheduling of PODs in the master node
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl get nodes -o wide
+
+# Kubernetes networking basics
+# The Kubernetes network model defines a “flat” network in which:
+# - Every pod get its own IP address.
+# - Pods on any node can communicate with all pods on all other nodes without NAT.
+# - This creates a clean, backwards-compatible model where pods can be treated much like VMs or physical hosts from the perspectives of port allocation, 
+#   naming, service discovery, load balancing, application configuration, and migration. Network segmentation can be defined using network policies 
+#   to restrict traffic within these base networking capabilities.
+
+# Within this model there’s quite a lot of flexibility for supporting different networking approaches and environments. 
+# The details of exactly how the network is implemented depend on the combination of CNI, network, and cloud provider plugins being used.
+
 
