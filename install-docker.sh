835d2611693a0153136e10bc9c3ec9ffaad29174
Arturo R. Quiroga <arturoqu@hotmail.ca>
2020-12-18 14:35:35 -0500

    enabling cgroups

--- install-docker.sh
+++ install-docker.sh
@@ -24,4 +24,49 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 sudo docker run hello-world
 
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker
+
+sudo docker info
+
+# Enable net.bridge.bridge-nf-call-iptables and -iptables6
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+$ sudo sysctl --system
+
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+# Create /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+
+
+# PRIOR TO RUNNING kubeadm!!!
+# Append the cgroups and swap options to the kernel command line
+# Note the space before "cgroup_enable=cpuset", to add a space after the last existing item on the line
+echo "Enabling Memory cgroup"
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+echo "Check that everythings is OK prior to initializing the cluster"
+sudo docker info
+echo "Rebooting now for changes to take effect!!"
+sudo reboot
+
+
 
--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -45,7 +45,12 @@ sudo systemctl restart kubelet
 # You can see all other CNIs in ==> https://kubernetes.io/docs/concepts/cluster-administration/networking/#calico
 # If you choose CALICO, then follow these instructions ==> https://docs.projectcalico.org/getting-started/kubernetes/quickstart
 
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+# PRIOR TO RUNNING kubeadm!!!
+# Append the cgroups and swap options to the kernel command line
+# Note the space before "cgroup_enable=cpuset", to add a space after the last existing item on the line
+sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+
+sudo kubeadm init --kubernetes-version=v1.18.2 --pod-network-cidr=192.168.0.0/16
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
